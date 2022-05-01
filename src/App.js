4ecaba6ca600cd57c074779854cd76dafd93b6d3
heejin lee <heejinlee07@gmail.com>
2022-05-01 19:54:19 +0900

    Add: breadcrumbs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Breadcrumbs = ({ children }) => {
+  return (
+    <nav aria-label='Breadcrumb'>
+      <BreadcrumbList>{children}</BreadcrumbList>
+    </nav>
+  );
+};
+
+const Crumb = ({ href, children }) => {
+  return (
+    <CrumbWrapper>
+      <CrumbLink href={href}>{children}</CrumbLink>
+    </CrumbWrapper>
+  );
+};
+
+const BreadcrumbList = styled.ol`
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+`;
+
+const CrumbWrapper = styled.li`
+  display: inline;
+  --spacing: 12px;
+
+  &:not(:first-of-type) {
+    margin-left: var(--spacing);
+
+    &:before {
+      display: inline-block;
+      content: '';
+      height: 0.8em;
+      border-right: 0.1em solid lightgray;
+      transform: rotate(15deg);
+      margin-right: var(--spacing);
+    }
+  }
+`;
+
+const CrumbLink = styled.a`
+  color: inherit;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: revert;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Breadcrumbs>
+      <Crumb href='/'>Home</Crumb>
+      <Crumb href='/living'>Living Room</Crumb>
+      <Crumb href='/living/couch'>Couches</Crumb>
+      <Crumb href='/living/couch/sectional'>Sectionals</Crumb>
+    </Breadcrumbs>
   );
 }
 
